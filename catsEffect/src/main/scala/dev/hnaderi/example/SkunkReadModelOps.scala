82c12718522a108114fae3ac18fb80cf5dc805d5
Niklas Uhrberg <niklas.uhrberg@gmail.com>
2024-09-17 20:40:02 +0200

    Skunk readmodel , first iteration with insert

--- catsEffect/src/main/scala/dev/hnaderi/example/SkunkReadModelOps.scala
+++ catsEffect/src/main/scala/dev/hnaderi/example/SkunkReadModelOps.scala
@@ -1,13 +1,46 @@
 package dev.hnaderi.example
-
-import cats.syntax.all.catsSyntaxApplicativeId
-import cats.effect.{Async, Resource}
+import cats.{Applicative, Monad}
+import cats.syntax.all.*
 import cats.effect.std.Console
+import cats.effect.{Async, Concurrent, Resource}
 import dev.hnaderi.example.metadata.Event
-import skunk.Session
-final case class SkunkReadModelOps[F[_]:Console](pool: Resource[F,Session[F]]) extends ReadModelOps[F] {
+import skunk.{Codec, Command, Session}
+import skunk.implicits.sql
+import skunk.*
+import skunk.codec.all.*
+import skunk.syntax.all.*
+import fs2.Compiler.Target.forConcurrent
+
+import java.util.UUID
+
+case class InsertMetadataRow( id: UUID,
+entityId: UUID ,
+parent: Option[UUID],
+createdBy: String,
+category: String)
+
+val codec: Codec[InsertMetadataRow] =
+  (uuid, uuid, uuid, varchar, varchar).tupled.imap {
+    case (id, entityId, parent, createdBy, category) => InsertMetadataRow(id, entityId, Option(parent),createdBy, category)
+  } { inr => (inr.id, inr.entityId, inr.parent.fold[UUID](UUID.randomUUID())(identity), inr.createdBy, inr.category) }
+
+def insertCommand: Command[InsertMetadataRow] =
+  sql"""
+    INSERT INTO metadata VALUES($codec)
+    """.command
+
+final case class SkunkReadModelOps[F[_]:Monad: Concurrent: Console](pool: Resource[F,Session[F]]) extends ReadModelOps[F] {
   override def process(event: Event): F[Unit] = {
-    Console[F].println(event)
+    val ini: InsertMetadataRow = event match {
+      case Event.Created(entityId, parent, category) => InsertMetadataRow(UUID.randomUUID(), entityId,
+        None, "default", category)
+    }
+    pool.use(s => for {
+      command <- s.prepare(insertCommand)
+      rowCount <- command.execute(ini)
+      _ <- Console[F].println(s"Executed insert with rowcount $rowCount")
+    } yield ())
+    //Console[F].println(event)
   }
 }
 
